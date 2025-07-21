cda8194e5269926d8a6f27bb3a55446ff6e6ee97
shazarnaeem <shazarnaeem5.com>
2025-07-21 17:55:22 +0500

    debug: add print statements for container startup

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+print("Script started...", flush=True)
+
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 import time
 from datetime import datetime, timedelta
 
+print("Chrome options set...", flush=True)
 # YouTube video URL
 url = "https://youtu.be/PwhkuMWqYN0"
 
@@ -17,20 +20,22 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 # ChromeDriver path for Railway Docker
 driver_path = "/usr/local/bin/chromedriver"
 
+print("Launching Chrome...", flush=True)
 # Start Chrome once
 service = Service(driver_path)
 driver = webdriver.Chrome(service=service, options=chrome_options)
+print("Chrome launched!", flush=True)
 
 # 6 ghante ka duration
 end_time = datetime.now() + timedelta(hours=6)
 
 while datetime.now() < end_time:
-    print(f"Opening {url}")
+    print(f"Opening {url}", flush=True)
     driver.get(url)
     # Video ko 1 minute tak play hone dein
     time.sleep(60)
-    print("Reloading video")
+    print("Reloading video", flush=True)
     time.sleep(10)
 
 driver.quit()
-print("6 ghante complete ho gaye. Script band ho gayi.")
+print("6 ghante complete ho gaye. Script band ho gayi.", flush=True)
